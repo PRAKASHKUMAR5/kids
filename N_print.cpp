5c612b9fdbbf2af108a0cd6f34f371922dda04c8
PRAKASHKUMAR5 <36396155+PRAKASHKUMAR5@users.noreply.github.com>
2018-02-20 16:45:14 +0530

    Add files via upload

---
+++ N_print.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int N;
+cout<<"Enter the N value"<<endl;
+cin>>N;
+if(N>=1)
+{
+    for(int i=0;i<N;i++)
+    {
+        cout << "Hello "<<endl;
+    }
+}
+return 0;
+}
