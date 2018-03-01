9aaef416342892beb3a4fe192a4be7eeb65815c8
PRAKASHKUMAR5 <36396155+PRAKASHKUMAR5@users.noreply.github.com>
2018-03-01 17:21:17 +0530

    Add files via upload

---
+++ Reverse_String.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   string s;
+   cout<<"Enter the String : "<<endl;
+   cin>>s; 
+   reverse(s.begin(),s.end());    
+   cout <<s;
+   return 0;
+}
