d2031d68c0cf5669721cfdeb2471db9f662bd8ee
PRAKASHKUMAR5 <36396155+PRAKASHKUMAR5@users.noreply.github.com>
2018-03-02 17:11:34 +0530

    Add files via upload

---
+++ P_S_up.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n;
+  char a[10];
+  cout<<"Enter the word & number"<<endl;
+  cin>>a>>n;
+  for(int i=0;i<n;i++)
+  {
+      cout<<a[i];
+  }  
+ return 0;
+}
