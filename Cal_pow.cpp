85009480e6383e1891723075c702ba818050be64
PRAKASHKUMAR5 <36396155+PRAKASHKUMAR5@users.noreply.github.com>
2018-03-03 17:53:07 +0530

    Create Cal_pow.cpp

---
+++ Cal_pow.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+int main()
+{
+ int n,m,a;
+ cout<<"Enter the num & pow values :"<<endl;
+ cin>>n>>m;
+ a=pow(n,m);
+ cout<<a;
+ return 0;
+}
