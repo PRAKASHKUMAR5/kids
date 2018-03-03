c957838eea474584c561c690114256e296fec57e
PRAKASHKUMAR5 <36396155+PRAKASHKUMAR5@users.noreply.github.com>
2018-03-03 17:40:30 +0530

    Create Kelvin.cpp

---
+++ Kelvin.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int temp,kelvin;
+   cout<<"Enter temperature :" <<endl;
+   cin>>temp;
+  	kelvin=temp+273.15;
+        cout<<kelvin;
+   return 0;
+  }
