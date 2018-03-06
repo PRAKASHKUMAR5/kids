9ec56eb657f1f33e36d135ce158f3f7a10ebc658
PRAKASHKUMAR5 <36396155+PRAKASHKUMAR5@users.noreply.github.com>
2018-03-06 17:52:13 +0530

    Create Cal_power_of_2.cpp

---
+++ Cal_power_of_2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter the number :"<<endl;
+    cin>>a;
+  if(a%2==0)
+  {
+      cout<<"Yes";
+  }
+  else
+  {
+      cout<<"No";
+  }
+  return 0;
+}
