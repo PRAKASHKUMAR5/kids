89642645d253d352f135e2223f79032a5f72fa35
PRAKASHKUMAR5 <36396155+PRAKASHKUMAR5@users.noreply.github.com>
2018-03-06 17:24:53 +0530

    Create Display_N_num.cpp

---
+++ Display_N_num.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    string a;
+    cout<<"Enter the string :"<<endl;
+    cin>>a;
+    cout<<"Enter the number of times String print :"<<endl;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cout<<a<<endl;
+    }
+    return 0;
+}
