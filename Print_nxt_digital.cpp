1d0fcb8ae41508be2fcb958c44d75b6a9f4719a1
PRAKASHKUMAR5 <36396155+PRAKASHKUMAR5@users.noreply.github.com>
2018-03-06 17:55:40 +0530

    Create Print_nxt_digital.cpp

---
+++ Print_nxt_digital.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter the number :"<<endl;
+    cin>>a;
+    if(a<=10000000)
+    {
+        cout<<a+1;
+    }
+  return 0;
+}
