baf4d1675046b639bcd95053292e6885844fa93b
PRAKASHKUMAR5 <36396155+PRAKASHKUMAR5@users.noreply.github.com>
2018-03-10 11:48:59 +0530

    Create F_b_n.cpp

---
+++ F_b_n.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int arr[25],c=0;
+  int size =sizeof(arr);
+  cout<<"Enter the binary num:";
+  cin>>arr[25];
+  for(int i=0;i<25;i++)
+  {
+      if(arr[i]==0 || arr[i]==1)
+      {
+          c++;
+      }
+  }
+  if(c==size)
+  {
+      cout<<"yes";
+  }
+  return 0;
+}
