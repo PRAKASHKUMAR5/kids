3d80695e35ba8fd4d63cf951044f4db226127417
PRAKASHKUMAR5 <36396155+PRAKASHKUMAR5@users.noreply.github.com>
2018-03-10 11:57:55 +0530

    Create A_c_E r O.cpp

---
+++ A_c_E r O.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a,b,c;
+  cout<<"Enter the Numbers :";
+  cin>>a>>b;
+  c=a+b;
+  if(c%2==0)
+  {
+      cout<<"Even";
+  }
+  else
+  {
+      cout<<"Odd";
+  }
+  return 0;
+}
