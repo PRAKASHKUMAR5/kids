527c178fe02fafa0a8c85b6811d70ddf2eb8a5b9
PRAKASHKUMAR5 <36396155+PRAKASHKUMAR5@users.noreply.github.com>
2018-03-12 17:22:08 +0530

    Create Diff_E_O.cpp

---
+++ Diff_E_O.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+int a,b,c;
+cout<<"Enter the input:"<<endl;
+cin>>a>>b;
+c=a-b;
+  
+  if(c%2==0)
+  {
+      cout<<"Even";
+  }
+  else
+  {
+      cout<<"Odd";
+  }
+ return 0;
+}
