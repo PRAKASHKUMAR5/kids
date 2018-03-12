ff5a676156d12ac77c28c8f00db635dcbebecae1
PRAKASHKUMAR5 <36396155+PRAKASHKUMAR5@users.noreply.github.com>
2018-03-12 17:58:36 +0530

    Create Count_paragraph.cpp

---
+++ Count_paragraph.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#include <string>
+using namespace std;
+int main()
+{
+string s;
+cout<<"Enter the input:"<<endl;
+getline(cin,s);
+int c=1;
+int l=s.length();
+char a[l];
+strcpy(a,s.c_str());
+for(int i=0;i<l;i++)
+{
+    if(a[i]=='.')
+    {
+        c++;
+    }
+}
+cout<<c;
+ return 0;
+}
