ebcd530f5d3e1fbf517d4c5e9e6bb5722b982a7e
PRAKASHKUMAR5 <36396155+PRAKASHKUMAR5@users.noreply.github.com>
2018-03-12 17:18:50 +0530

    Create Mul_by_7.cpp

---
+++ Mul_by_7.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+int a;
+cout<<"Enter the input:"<<endl;
+cin>>a;
+  
+  if(a%7==0)
+  {
+      cout<<"Yes";
+  }
+  else
+  {
+      cout<<"No";
+  }
+ return 0;
+}
